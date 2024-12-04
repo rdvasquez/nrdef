a376fdbe80d9856af9cb04a74b9a066583573d95
ROWENA VASQUEZ <vasquezrd@gmail.com>
2024-12-04 11:49:14 +0000

    winwork

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}

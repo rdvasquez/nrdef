b4d77e78cd5a838b535e3972e03b9075f5af6e90
subhadra30 <k.subhadra@gmail.com>
2025-01-03 14:05:47 +0000

    functional navbar

---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+export default function ContactForm() {
+  return (
+    <>
+      <h1>This is Contact page</h1>
+      <h2>Contact us here</h2>
+    </>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ export default async function Header() {
 
   return (
     <header className="header">
-     <div className="image-wrapper">
+      <div className="image-wrapper">
         <Image
           className="heroImage"
           src="/header.png"
@@ -31,7 +31,6 @@ export default async function Header() {
         <Image
           className="logo"
           src="/logo.png"
-
           width={200}
           height={200}
           alt="logo"
@@ -41,8 +40,9 @@ export default async function Header() {
           Romans 10:15
         </span>
       </div>
-      <nav className="nav">
 
+      {/* <nav className="nav">
+<Image
           width={250}
           height={250}
           objectFit="cover"
@@ -51,9 +51,8 @@ export default async function Header() {
         <span className="verse">
           “How beautiful are the feet of those who bring good news” Romans 10:15
         </span>
-      </div>
+      </div> */}
       <nav className="navbar">
-
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/resources">Resources</Link>
@@ -61,11 +60,9 @@ export default async function Header() {
         <Link href="/contact">Contact</Link>
         <div className="signIn">
           <SignedIn>
-
             <span>
               Welcome {user?.firstName} {user?.lastName}
             </span>
-
             <UserButton />
           </SignedIn>
           <SignedIn>
@@ -75,10 +72,9 @@ export default async function Header() {
             <SignInButton />
           </SignedOut>
 
-          <button className="joinUs">Join us</button>
-
-          <button className="Joinus">Join us</button>
-
+          <button className="joinUs">
+            <Link href="/users">Join us</Link>
+          </button>
         </div>
       </nav>
     </header>

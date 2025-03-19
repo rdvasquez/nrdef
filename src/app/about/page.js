351fb030f3c7592f72a0c6e30c5a18abeeb213a9
ROWENA VASQUEZ <vasquezrd@gmail.com>
2025-03-19 16:58:51 +0000

    WinNewUpdate

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,23 +2,20 @@ import styles from "./aboutpage.module.css";
 
 export default function About() {
   return (
-
     <div className={styles.aboutContainer}>
       <div className={styles.content}>
         <h1 className={styles.content1}>
           Who is Diocesan Evangelical Fellowship?
         </h1>
         <p className={styles.description}>
-
-          The Diocesan Evangelical Fellowship is a network representing
-          approximately 20 Anglican Evangelical churches across Norwich,
-          Norfolk, and the Waveney Valley. Our membership currently consists of
-          71 individuals.
+          The Diocesan Evangelical Fellowship is a network of around 20
+          evangelical Anglican churches in Norfolk, and the Waveney Valley
+          united by faith, love, and a commitment to sharing the good news in
+          our communities. Our membership currently consists of 71 individuals.
         </p>
 
         <h2 className={styles.content2}>Our purpose as an organisation is:</h2>
-
-        <ul>
+        <ul className="list">
           <li>To unite evangelical ministers and congregation members</li>
           <li>To promote evangelical faith, mission, and ministry</li>
           <li>
--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -1,13 +1,58 @@
+import Image from "next/image";
+import "./resourcespage.module.css";
+
 export default function ResourcesPage() {
   return (
-    <div className="Resources">
+    <div className="resources-container">
       <div>
-        <h1>Norwich Diocesan Evangelical Fellowship?</h1>
+        <h1 className="community">What we do for the community?</h1>
+        <div className="outreachimage">
+          <figure className="outreach">
+            <Image
+              className="outreach"
+              src="/outreach.png"
+              width={400}
+              height={300}
+              alt="youth outreach"
+            />
+            <figcaption className="caption">Youth Outreach Program</figcaption>
+          </figure>
+
+          <figure className="outreach1">
+            <Image
+              className="outreach1"
+              src="/outreach1.png"
+              width={400}
+              height={300}
+              alt="community outreach"
+            />
+            <figcaption className="caption">
+              Community Outreach Activities
+            </figcaption>
+          </figure>
+
+          <figure className="outreach2">
+            <Image
+              className="outreach3"
+              src="/outreach3.png"
+              width={400}
+              height={300}
+              alt="donation"
+            />
+            <figcaption className="caption">
+              Donation Drives for Those in Need
+            </figcaption>
+          </figure>
+        </div>
         <span className="description">
-          The Diocesan Evangelical Fellowship is a network representing
-          approximately 20 Anglican Evangelical churches across Norwich,
-          Norfolk, and the Waveney Valley. Our membership currently consists of
-          71 individuals.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          convallis mauris vel mi elementum, ac vestibulum nulla sagittis.
+          Integer in neque nec nisl tincidunt luctus. Vivamus viverra sem ac
+          orci vestibulum, in pharetra ante sollicitudin. Curabitur vitae dolor
+          nec lorem tincidunt malesuada. Ut sit amet ullamcorper nisi. Fusce
+          tincidunt eros at dui tincidunt, sit amet pretium ex mollis.
+          Suspendisse potenti. Proin viverra nisi non libero scelerisque, at
+          fermentum ligula ullamcorper.
         </span>
       </div>
     </div>

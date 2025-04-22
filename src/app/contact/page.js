63b44b1715ea138f61e60383ba7702cd559588d1
ROWENA VASQUEZ <vasquezrd@gmail.com>
2025-04-22 11:42:46 +0100

    MyFooter-Contact-Updates

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,14 +1,27 @@
 import ContactForm from "@/components/Contact";
 import NavBarBurger from "@/components/HamburgerMenu.jsx";
+import styles from "./page.module.css";
 
 export default function ContactPage() {
   return (
-    <>
-      <h1>This is Contact page</h1>
-      <h2>Contact us here</h2>
-      <div>
+    <div className={styles.contactWrapper}>
+      <header className={styles.header}>
         <NavBarBurger />
-      </div>
-    </>
+      </header>
+
+      <main className={styles.mainContent}>
+        <div className={styles.textSection}>
+          <h1>Contact Us</h1>
+          <p>
+            We’d love to hear from you. Whether you have a question, feedback,
+            or just want to connect, feel free to drop us a message below.
+          </p>
+        </div>
+
+        <div className={styles.formSection}>
+          <ContactForm />
+        </div>
+      </main>
+    </div>
   );
 }
